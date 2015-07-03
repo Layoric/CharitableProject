1031f1d1dfdb19a645baa2cadfbfefe8c8066780
Adam Baxter <voltagex@voltagex.org>
2015-07-04 01:20:24 +1000

    Import other data. Sleep.

--- CharitableProject/CharitableProject.Tests/UnitTests.cs
+++ CharitableProject/CharitableProject.Tests/UnitTests.cs
@@ -42,7 +42,7 @@ namespace CharitableProject.Tests
         }
 
         [Test]
-        public void TestMethod1()
+        public void ImportCharityData()
         {
             var dbConnectionFactory = appHost.Container.Resolve<IDbConnectionFactory>();
             using (var db = dbConnectionFactory.OpenDbConnection())
@@ -53,18 +53,39 @@ namespace CharitableProject.Tests
             {
                 string currentPath = AppDomain.CurrentDomain.BaseDirectory;
 
-                var filestream = File.OpenRead(Path.Combine(currentPath, ConfigurationManager.AppSettings["DataPath"]));
+                var filestream = File.OpenRead(Path.Combine(currentPath, ConfigurationManager.AppSettings["CharityPath"]));
                 TextReader textreader = new StreamReader(filestream);
                 var reader = new CsvHelper.CsvReader(textreader);
 
                 var lol = reader.GetRecords<dynamic>();
-
                 var charityJson = lol.ToJson();
-
                 var data = JsonSerializer.DeserializeFromString<List<Charity>>(charityJson);
 
                 db.InsertAll(data);
+            }
+        }
 
+        [Test]
+        public void ImportCharityMetadata()
+        {
+            var dbConnectionFactory = appHost.Container.Resolve<IDbConnectionFactory>();
+            using (var db = dbConnectionFactory.OpenDbConnection())
+            {
+                db.DropAndCreateTable<CharityMetadata>();
+            }
+            using (var db = dbConnectionFactory.OpenDbConnection())
+            {
+                string currentPath = AppDomain.CurrentDomain.BaseDirectory;
+
+                var filestream = File.OpenRead(Path.Combine(currentPath, ConfigurationManager.AppSettings["CharityMetadataPath"]));
+                TextReader textreader = new StreamReader(filestream);
+                var reader = new CsvHelper.CsvReader(textreader);
+
+                var lol = reader.GetRecords<dynamic>();
+                var charityJson = lol.ToJson();
+                var data = JsonSerializer.DeserializeFromString<List<CharityMetadata>>(charityJson);
+
+                db.InsertAll(data);
             }
         }
     }
